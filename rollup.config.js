c66f0f417f157858a263e107fd197dddbb64ab98
Laurent Kvb <laurent@fullstackrebels.nl>
2020-06-05 13:30:49 +0200

    Typescript support added

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,21 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import typescript from 'rollup-plugin-typescript';
 import pkg from './package.json';
 
 export default [
 	// browser-friendly UMD build
 	{
-		input: 'src/main.js',
+		input: 'src/main.ts',
 		output: {
 			name: 'howLongUntilLunch',
 			file: pkg.browser,
 			format: 'umd'
 		},
 		plugins: [
-			resolve(), // so Rollup can find `ms`
-			commonjs() // so Rollup can convert `ms` to an ES module
+			resolve(),   // so Rollup can find `ms`
+			commonjs(),  // so Rollup can convert `ms` to an ES module
+			typescript() // so Rollup can convert TypeScript to JavaScript
 		]
 	},
 
@@ -21,11 +23,14 @@ export default [
 	// (We could have three entries in the configuration array
 	// instead of two, but it's quicker to generate multiple
 	// builds from a single configuration where possible, using
-	// an array for the `output` option, where we can specify
+	// an array for the `output` option, where we can specify 
 	// `file` and `format` for each target)
 	{
-		input: 'src/main.js',
+		input: 'src/main.ts',
 		external: ['ms'],
+		plugins: [
+			typescript() // so Rollup can convert TypeScript to JavaScript
+		],
 		output: [
 			{ file: pkg.main, format: 'cjs' },
 			{ file: pkg.module, format: 'es' }
