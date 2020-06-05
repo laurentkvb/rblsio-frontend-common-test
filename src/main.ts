79df1ffd820ef7fdf88d8b9d6a09d5b3db7913bd
Laurent Kvb <laurent@fullstackrebels.nl>
2020-06-05 13:58:01 +0200

    Testing

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import lunchtime from './lunchtime.js';
 import millisecondsUntil from './millisecondsUntil.js';
 import ColumnsWrapper from "./ColumnsWrapper";
 
-const howLongUntilLunch = (hours: number = 12, minutes: number = 30): string => {
-	const millisecondsUntilLunchTime = millisecondsUntil(lunchtime(hours, minutes));
-	return ms(millisecondsUntilLunchTime, { long: true });
-}
-export { ColumnsWrapper, howLongUntilLunch };
+// const howLongUntilLunch = (hours: number = 12, minutes: number = 30): string => {
+// 	const millisecondsUntilLunchTime = millisecondsUntil(lunchtime(hours, minutes));
+// 	return ms(millisecondsUntilLunchTime, { long: true });
+// }
+export { ColumnsWrapper };
