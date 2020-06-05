a1430770732260407ea69b23ff85aa872244d27d
Laurent Kvb <laurent@fullstackrebels.nl>
2020-06-05 14:41:31 +0200

    Added declaration file generation with yarn build

--- rollup.config.js
+++ rollup.config.js
@@ -1,39 +1,18 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import typescript from 'rollup-plugin-typescript';
-import pkg from './package.json';
+// import resolve from 'rollup-plugin-node-resolve';
+// import commonjs from 'rollup-plugin-commonjs';
+// import pkg from './package.json';
+import typescript from '@rollup/plugin-typescript';
+import ts from "@wessberg/rollup-plugin-ts";
 
-export default [
-	// browser-friendly UMD build
-	{
-		input: 'src/main.ts',
-		output: {
-			name: 'index',
-			file: pkg.browser,
-			format: 'umd'
-		},
-		plugins: [
-			resolve(),   // so Rollup can find `ms`
-			commonjs(),  // so Rollup can convert `ms` to an ES module
-			typescript() // so Rollup can convert TypeScript to JavaScript
-		]
+export default {
+	input: 'src/main.ts',
+	output: {
+		dir: './dist',
+		format: 'cjs'
 	},
-
-	// CommonJS (for Node) and ES module (for bundlers) build.
-	// (We could have three entries in the configuration array
-	// instead of two, but it's quicker to generate multiple
-	// builds from a single configuration where possible, using
-	// an array for the `output` option, where we can specify
-	// `file` and `format` for each target)
-	{
-		input: 'src/main.ts',
-		external: ['ms'],
-		plugins: [
-			typescript() // so Rollup can convert TypeScript to JavaScript
-		],
-		output: [
-			{ file: pkg.main, format: 'cjs' },
-			{ file: pkg.module, format: 'es' }
-		]
-	}
-];
+	plugins: [
+		ts({
+		}, 			typescript(),
+		)
+	]
+};
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,2 @@
-import ms from 'ms';
-import lunchtime from './lunchtime.js';
-import millisecondsUntil from './millisecondsUntil.js';
-import { TestWrapper } from "./TestWrapper/TestWrapper";
 
-// const howLongUntilLunch = (hours: number = 12, minutes: number = 30): string => {
-// 	const millisecondsUntilLunchTime = millisecondsUntil(lunchtime(hours, minutes));
-// 	return ms(millisecondsUntilLunchTime, { long: true });
-// }
-export { TestWrapper };
+export const greet = (name = "sir") => console.log(`Hello, world! ${name}`)
