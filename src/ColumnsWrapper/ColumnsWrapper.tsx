b6727b30c289f5f1d8eafb3cc3ce67ac1ff05d60
Laurent Kvb <laurent@fullstackrebels.nl>
2020-06-05 13:49:44 +0200

    Columnswrapper test

---
+++ src/ColumnsWrapper/ColumnsWrapper.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+// import Flex from "@components/Flex";
+// import { SystemProps } from "@models/styledSystem";
+
+// interface Props extends SystemProps {
+//   children: ReactNode;
+//   fullWidth?: boolean;
+//   backgroundColor?: string | string[];
+//   debugging?: boolean;
+// }
+
+
+/**
+ * 12 column grid wrapper
+ * @param children React Nodes content that have to be in the content
+ * @param fullWidth if set to true, then the component will be shown without
+ * paddingX variables enables.
+ * @param debugging is true, then columns gets colored for debugging purposes
+ * @param backgroundColor
+ * @param props are optional parameters to change properties of the parents
+ * CSS properties.
+ */
+const ColumnsWrapper = () => (
+    <div><h1>Display columns wrapper over here</h1></div>
+);
+
+export default ColumnsWrapper;
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import ms from 'ms';
 import lunchtime from './lunchtime.js';
 import millisecondsUntil from './millisecondsUntil.js';
+import ColumnsWrapper from "./ColumnsWrapper";
 
-export default function howLongUntilLunch(hours: number = 12, minutes: number = 30): string {
+const howLongUntilLunch = (hours: number = 12, minutes: number = 30): string => {
 	const millisecondsUntilLunchTime = millisecondsUntil(lunchtime(hours, minutes));
 	return ms(millisecondsUntilLunchTime, { long: true });
 }
+export { ColumnsWrapper, howLongUntilLunch };
