984d5d1c49e117f96e8f59d42f421952a53c22f2
Laurent Kvb <laurent@fullstackrebels.nl>
2020-06-08 11:14:11 +0200

    Trying to work with box component

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import { TestWrapper } from "./TestWrapper/TestWrapper";
 import Box1 from "./Box1/Box1";
 import KolommenWrapper  from "./KolommenWrapper/KolommenWrapper";
+import {BoxComponent}  from "./Box1/box.styles.js";
 
 export const greet = (name = "sir") => console.log(`Hello, world! ${name}`)
 
-export { TestWrapper,KolommenWrapper, Box1 }
+export { TestWrapper,KolommenWrapper, Box1, BoxComponent }
