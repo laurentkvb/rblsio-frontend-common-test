a342c6bead491d4308acb4fbd1cf55d90e723930
Laurent Kvb <laurent@fullstackrebels.nl>
2020-06-08 11:04:53 +0200

    Trying to export box component

--- rollup.config.js
+++ rollup.config.js
@@ -9,15 +9,16 @@ import resolve from '@rollup/plugin-node-resolve';
 
 export default {
 	input: 'src/index.ts',
+	format     : "iife",
 	output: {
 		dir: './build',
-		format: 'cjs'
+		format: 'cjs',
 	},
 	plugins: [
 		ts({}),
 		typescript(),
 		commonjs({
-			include: [ "./index.js", "node_modules/**" ], // Default: undefined
+			// include: [ "./index.js", "node_modules/**" ], // Default: undefined
 			// if true then uses of `global` won't be dealt with by this plugin
 			ignoreGlobal: false, // Default: false
 
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { TestWrapper } from "./TestWrapper/TestWrapper";
+import Box from "./Box/Box";
 import KolommenWrapper  from "./KolommenWrapper/KolommenWrapper";
 
 export const greet = (name = "sir") => console.log(`Hello, world! ${name}`)
 
-export { TestWrapper,KolommenWrapper }
+export { TestWrapper,KolommenWrapper, Box }
