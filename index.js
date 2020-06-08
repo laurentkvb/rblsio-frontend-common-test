9f6306178b543acdab4d68704034b004d13f0637
Laurent Kvb <laurent@fullstackrebels.nl>
2020-06-08 08:57:43 +0200

    Put the code in the root to access

---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var greet = function (name) {
+    if (name === void 0) { name = "sir"; }
+    return console.log("Hello, world! " + name);
+};
+
+exports.greet = greet;
