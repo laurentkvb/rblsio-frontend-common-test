4998ce3cbc29670257ec2aa97ba955a07848e420
Laurent Kvb <laurent@fullstackrebels.nl>
2020-06-09 07:34:14 +0200

    Box1 with theme provider

--- src/Box/Box1.tsx
+++ src/Box/Box1.tsx
@@ -1,11 +1,18 @@
 import React, { ReactNode } from "react";
 import { SystemProps } from "../models/styledSystem";
 import { BoxComponent } from "./box.styles";
+import {ThemeProvider} from "styled-components";
+
+// @ts-ignore
+import theme from "../styles/theme"; // TODO: REMOVE THIS AND ADD ESLINTJS FOR RECOGNIZING OF FILES
 
 export interface BoxProps extends SystemProps {
   children?: ReactNode;
 }
 
-const Box1 = (props: BoxProps) => <BoxComponent {...props}>{props.children}</BoxComponent>;
+const Box1 = (props: BoxProps) =>
+    <ThemeProvider theme={theme}>
+      <BoxComponent {...props}>{props.children}</BoxComponent>
+    </ThemeProvider>;
 
 export default Box1;
